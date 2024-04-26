d28b00eecc756b351a5b0973ed84d916ad1afb5d
Lachlan <lachlanmuddle@gmail.com>
2024-04-26 16:52:14 +1000

    Journey Deleter finished

--- src/SystemInterface.java
+++ src/SystemInterface.java
@@ -2,22 +2,41 @@ import java.util.Scanner;
 
 public class SystemInterface {
 
-    private SmartCard smartCard1, smartCard2, smartCard3;
+    private SmartCard smartCard1 = new SmartCard();
+    private SmartCard smartCard2 = new SmartCard();
+    private SmartCard smartCard3 = new SmartCard();
+    private Journey InvalidJourney = new Journey();
+
     private void run() {
+        InvalidJourney.setJourneyID(0);
+        InvalidJourney.setStartOfJourney(0);
+        InvalidJourney.setEndOfJourney(0);
+        InvalidJourney.setTransportMode("NaN");
+        InvalidJourney.setDistanceOfJourney();
+
+        smartCard1.setJourney1(InvalidJourney);
+        smartCard1.setJourney2(InvalidJourney);
+        smartCard1.setJourney3(InvalidJourney);
+        smartCard2.setJourney1(InvalidJourney);
+        smartCard2.setJourney2(InvalidJourney);
+        smartCard2.setJourney3(InvalidJourney);
+        smartCard3.setJourney1(InvalidJourney);
+        smartCard3.setJourney2(InvalidJourney);
+        smartCard3.setJourney3(InvalidJourney);
+
 
         Scanner keyboard = new Scanner(System.in);
 
-        SmartCard card = new SmartCard();
-        card.setType('a');
-        card = JourneySetter(card, keyboard);
-        System.out.println(card.getJourney1().getJourneyID());
+        smartCard1.setType('a');
+        smartCard1 = JourneySetter(smartCard1, keyboard);
+        JourneyDeleter(smartCard1, keyboard);
     }
 
     public SmartCard JourneySetter(SmartCard card, Scanner keyboard){
         SmartCard subCard = card;
         if (card.getType() == 'c') {
             System.out.println("You can set 1 Journey on this card.");
-            System.out.print("How many journeys would you like to set");
+            System.out.print("How many journeys would you like to set? ");
             int numJourneys = keyboard.nextInt();
             if (numJourneys == 1) {
                 int JourneyID = 0;
@@ -332,9 +351,113 @@ public class SystemInterface {
         }
     }
 
+    public SmartCard JourneyDeleter(SmartCard card, Scanner keyboard) {
+        SmartCard subCard = card;
+        int J1 = card.getJourney1().getJourneyID();
+        int J2 = card.getJourney2().getJourneyID();
+        int J3 = card.getJourney3().getJourneyID();
+        if (J1 != 0 && J2 != 0 && J3 != 0) {
+            System.out.println("The journeys on this card are: " + J1 + ", " + J2 + " and " + J3 + ".");
+            System.out.print("Which journey would you like to delete?");
+            int deletion = keyboard.nextInt();
+            if (deletion == J1) {
+                subCard.setJourney1(InvalidJourney);
+                System.out.println("Journey " + J1 + " has been deleted.");
+                return subCard;
+            }
+            if (deletion == J2) {
+                subCard.setJourney2(InvalidJourney);
+                System.out.println("Journey " + J2 + " has been deleted.");
+                return subCard;
+            }
+            if (deletion == J3) {
+                subCard.setJourney3(InvalidJourney);
+                System.out.println("Journey " + J3 + " has been deleted.");
+                return subCard;
+            }
+            return JourneyDeleter(card, keyboard);
+        } else if (J1 != 0 && J2 != 0) {
+            System.out.println("The journeys on this card are: " + J1 + " and " + J2);
+            System.out.print("Which journey would you like to delete?");
+            int deletion = keyboard.nextInt();
+            if (deletion == J1) {
+                subCard.setJourney1(InvalidJourney);
+                System.out.println("Journey " + J1 + " has been deleted.");
+                return subCard;
+            }
+            if (deletion == J2) {
+                subCard.setJourney2(InvalidJourney);
+                System.out.println("Journey " + J2 + " has been deleted.");
+                return subCard;
+            }
+            return JourneyDeleter(card, keyboard);
+        } else if (J1 != 0 && J3 != 0) {
+            System.out.println("The journeys on this card are: " + J1 + " and " + J3);
+            System.out.print("Which journey would you like to delete?");
+            int deletion = keyboard.nextInt();
+            if (deletion == J1) {
+                subCard.setJourney1(InvalidJourney);
+                System.out.println("Journey " + J1 + " has been deleted.");
+                return subCard;
+            }
+            if (deletion == J3) {
+                subCard.setJourney2(InvalidJourney);
+                System.out.println("Journey " + J3 + " has been deleted.");
+                return subCard;
+            }
+            return JourneyDeleter(card, keyboard);
+        } else if (J2 != 0 && J3 != 0) {
+            System.out.println("The journeys on this card are: " + J2 + " and " + J3);
+            System.out.print("Which journey would you like to delete?");
+            int deletion = keyboard.nextInt();
+            if (deletion == J2) {
+                subCard.setJourney1(InvalidJourney);
+                System.out.println("Journey " + J2 + " has been deleted.");
+                return subCard;
+            }
+            if (deletion == J3) {
+                subCard.setJourney2(InvalidJourney);
+                System.out.println("Journey " + J3 + " has been deleted.");
+                return subCard;
+            }
+            return JourneyDeleter(card, keyboard);
+        } else if (J1 != 0) {
+            System.out.println("The journeys on this card are: " + J1);
+            System.out.print("Which journey would you like to delete?");
+            int deletion = keyboard.nextInt();
+            if (deletion == J1) {
+                subCard.setJourney1(InvalidJourney);
+                System.out.println("Journey " + J1 + " has been deleted.");
+                return subCard;
+            }
+            return JourneyDeleter(card, keyboard);
+        } else if (J2 != 0) {
+            System.out.println("The journeys on this card are: " + J2);
+            System.out.print("Which journey would you like to delete?");
+            int deletion = keyboard.nextInt();
+            if (deletion == J2) {
+                subCard.setJourney1(InvalidJourney);
+                System.out.println("Journey " + J2 + " has been deleted.");
+                return subCard;
+            }
+            return JourneyDeleter(card, keyboard);
+        } else if (J3 != 0) {
+            System.out.println("The journeys on this card are: " + J3);
+            System.out.print("Which journey would you like to delete?");
+            int deletion = keyboard.nextInt();
+            if (deletion == J3) {
+                subCard.setJourney1(InvalidJourney);
+                System.out.println("Journey " + J3 + " has been deleted.");
+                return subCard;
+            }
+            return JourneyDeleter(card, keyboard);
+        }
+        System.out.println("There are no journeys on this card.");
+        return card;
+    }
+
     public static void main(String[] args) {
         SystemInterface systemUI = new SystemInterface();
         systemUI.run();
-
     }
 }
